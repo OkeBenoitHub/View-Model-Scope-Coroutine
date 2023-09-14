44437718fed157dc41582859c20b125336e08f0f
preslybenoit <okebenoithub@gmail.com>
2023-09-14 12:36:31 +0100

    View Model Scope Coroutine

---
+++ app/src/main/java/com/anushka/viewmodelscopedemo/model/UserRepository.kt
@@ -0,0 +1,15 @@
+package com.anushka.viewmodelscopedemo.model
+
+import kotlinx.coroutines.delay
+
+class UserRepository {
+    suspend fun getUsers(): List<User> {
+        delay(8000)
+        return listOf(
+            User(1, "Sam"),
+            User(2, "Taro"),
+            User(3, "Jane"),
+            User(4, "Amy")
+        )
+    }
+}
